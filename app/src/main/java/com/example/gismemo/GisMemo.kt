25641087ced81522243a2b691dbe007839875700
UnChil Yeo <80609786+unchil@users.noreply.github.com>
2023-08-07 17:29:39 +0900

    Create README.md

--- app/src/main/java/com/example/gismemo/GisMemo.kt
+++ app/src/main/java/com/example/gismemo/GisMemo.kt
@@ -5,7 +5,7 @@ import coil.ImageLoader
 import coil.ImageLoaderFactory
 import com.example.gismemo.api.UnsplashSizingInterceptor
 
-//@HiltAndroidApp
+
 class GisMemo : Application(), ImageLoaderFactory {
     override fun newImageLoader(): ImageLoader {
         return ImageLoader.Builder(this)
